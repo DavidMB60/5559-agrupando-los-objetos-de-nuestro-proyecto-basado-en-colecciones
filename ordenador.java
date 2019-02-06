6c3a5459a8356356bb2085be0af1531d7682c057
MarcosMC17 <marcosmoranc@gmail.com>
2019-02-06 13:11:47 +0100

    Commit 2: Creados metodos setter y getter para los atributos

--- ordenador.java
+++ ordenador.java
@@ -16,4 +16,43 @@ public class ordenador{
         tieneSO = SO; 
         fechaVenta = LocalDate.parse(fechaVendido);
     }
+
+    public void setMarca(String marcaPC){
+        marca = marcaPC;
+    }
+
+    public void setGeneracion(int generacion){
+        cpuGen = generacion;
+    }
+
+    public void setTieneSO(boolean SO){
+        tieneSO = SO;
+    }
+
+    public void setFechaVenta(String fechaNueva){
+        fechaVenta = LocalDate.parse(fechaNueva);
+    }
+
+    public String getMarca(){
+        return marca;
+    }
+
+    public int getCpuGen(){
+        return cpuGen;
+    }
+
+    public String getTieneSO(){
+        String cadenaADevolver = "";
+        if(tieneSO){
+            cadenaADevolver = "Tiene SO";
+        }
+        else{
+            cadenaADevolver = "No tiene SO";
+        }
+        return cadenaADevolver;
+    }
+    
+    public LocalDate getFechaVenta(){
+        return fechaVenta;
+    }
 }
